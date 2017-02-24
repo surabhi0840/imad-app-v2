8e2993ca09f46d6d44b80c4851ddad178d25ce6c
surabhi0840 <kediyasurabhi@gmail.com>
2017-02-24 11:37:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/test-db', function (req, res) {
       if(err){
           res.status(500).send(err.tostring());
       }
-      else{res.send(JSON.stringify(result));
+      else{res.send(JSON.stringify(result.rows));
   }
 });
 });
